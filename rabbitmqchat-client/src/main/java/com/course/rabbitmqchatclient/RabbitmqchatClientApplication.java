8907540c20a4b294b6514a729ba4cfe2b206f975
ohj4312 <ohj4312@naver.com>
2024-04-18 14:40:16 +0900

    chore: commit 되지 않은 파일 업로드

---
+++ rabbitmqchat-client/src/main/java/com/course/rabbitmqchatclient/RabbitmqchatClientApplication.java
@@ -0,0 +1,13 @@
+package com.course.rabbitmqchatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqchatClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqchatClientApplication.class, args);
+	}
+
+}
---
+++ rabbitmqchat-client/src/test/java/com/course/rabbitmqchatclient/RabbitmqchatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.rabbitmqchatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqchatClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
